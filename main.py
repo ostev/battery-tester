cc96e949e4f5089a92fec2268e1a277533f3dc50
Oscar Stevens <oscar_stevens@icloud.com>
2021-08-06 07:23:39 +1000

    Start playing with time

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Bonjour!")
+import json
+import time
+
+def get_prev_time():
+    prev_time = None
+    try:
+        file = open("/prev_time", "r")
+        prev_time = file.read()
+    except:
+        prev_time = time.time()
+    
+    return prev_time
--- run_windows.ps1
+++ run_windows.ps1
@@ -1,4 +1,6 @@
 # This assumes that your CIRCUITPY drive is mounted as F.
 
 rm F:\code.py
+rm F:\boot.py
 cp main.py F:\code.py
+cp boot.py F:\boot.py
