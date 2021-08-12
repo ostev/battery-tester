a412a30593eb9b6f5bd72bf4d268bc46bab9e427
Oscar Stevens <oscar_stevens@icloud.com>
2021-08-12 12:33:50 +1000

    Not. Working.

--- main.py
+++ main.py
@@ -6,32 +6,35 @@ def prev_sample_time():
     prev_time = None
     try:
         file = open("/prev_time", "r")
-        prev_time = file.read()
+        prev_time = int(file.read())
+        print("file" + file.read())
         file.close()
     except:
-        prev_time = time.time()
+        prev_time = int(time.time()) - 1000000000
     return prev_time
-    
 
 def next_sample_time(prev_time):
-    next = ceil(prev_time / 60.0) * 60.0
+    next = (ceil(prev_time / 60) * 60) + 60
     return next
 
 def wait_until_next_sample_time():
+    print(time.time())
     prev = prev_sample_time()
     next = next_sample_time(prev)
     print("prev " + str(prev))
-    print("next " + str(next))
-    print(next - prev)
-    sleep(next - prev) # Why does this error?
+    print(next)
+    print(str(next - prev)) # TODO: Why is this negative?
+    sleep(next - prev)
     # sleep(1)
 
     file = open("/prev_time", "w")
-    file.write(next)
+    file.write(str(next))
+    file.close()
 
 def main():
     while True:
         wait_until_next_sample_time()
         print("sample")
+        sleep(0.999)
 
 main()
